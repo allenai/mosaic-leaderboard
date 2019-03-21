52cee3c89a3991730b312e528093a9cf500c75c8
Michael Schmitz <schmmd@cs.washington.edu>
2019-03-21 09:19:21 -0700

    Update random_baseline.py

--- anli/baselines/random-baseline/random_baseline.py
+++ anli/baselines/random-baseline/random_baseline.py
@@ -3,22 +3,23 @@ import json
 from typing import List
 import random
 
-
+# Parse the input file from JSONL to a list of dictionaries.
 def read_jsonl_lines(input_file: str) -> List[dict]:
     with open(input_file) as f:
         lines = f.readlines()
         return [json.loads(l.strip()) for l in lines]
 
 
-def main(args):
-    input_file = args.input_file
-    output_file = args.output_file
-
+def main(input_file, output_file):
+    # Read the records from the test set.
     test_records = read_jsonl_lines(input_file)
-    pred_answers = [random.choice(["1", "2"]) for r in  test_records]
 
+    # Make predictions for each example in the test set.
+    predicted_answers = [random.choice(["1", "2"]) for r in  test_records]
+
+    # Write the predictions to the output file.
     with open(output_file, "w") as f:
-        for p in pred_answers:
+        for p in predicted_answers:
             f.write(p)
             f.write("\n")
         f.close()
@@ -27,12 +28,11 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='A random baseline.')
-    # Required Parameters
-    parser.add_argument('--input_file', type=str, help='Location of input file', default=None)
-    parser.add_argument('--output_file', type=str, help='Location of predictions', default=None)
+    parser.add_argument('--input_file', type=str, required=True, help='Location of test records', default=None)
+    parser.add_argument('--output_file', type=str, required=True, help='Location of predictions', default=None)
 
     args = parser.parse_args()
     print('====Input Arguments====')
     print(json.dumps(vars(args), indent=2, sort_keys=True))
     print("=======================")
-    main(args)
+    main(args.input_file, args.output_file)
