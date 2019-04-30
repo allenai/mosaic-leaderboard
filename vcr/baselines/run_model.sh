5765d64df217dd2bb1f9c201c8bdbe660788e096
chandrab <chandrab@allenai.org>
2019-04-29 21:59:20 -0700

    Fix args in run model script

--- vcr/baselines/run_model.sh
+++ vcr/baselines/run_model.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python random_baseline.py --input-file /data/anli.jsonl --output-file /results/predictions.lst
+python random_baseline.py --input-dir /data/ --output-file /results/predictions.csv
