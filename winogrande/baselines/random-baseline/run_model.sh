864debbbdcb95e754a1653899d1f680395d3af1f
keisks <keisks5@gmail.com>
2019-09-27 12:18:49 -0700

    correct input-file name

--- winogrande/baselines/random-baseline/run_model.sh
+++ winogrande/baselines/random-baseline/run_model.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python random_baseline.py --input-file /data/anli.jsonl --output-file /results/predictions.lst
+python random_baseline.py --input-file /data/dev.jsonl --output-file /results/predictions.lst
